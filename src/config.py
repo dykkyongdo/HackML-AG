4adecd14cc93f6a51997e3cf09f070da84234c30
kirillous <kirill.markin18@gmail.com>
2026-02-03 00:52:08 -0800

    Added features and assessed importance using simplistic LightGBM

--- src/config.py
+++ src/config.py
@@ -10,4 +10,7 @@ CATEGORICAL_COL = "type"
 
 CLASS_WEIGHT = {0: 1.0, 1: 3.0, 2: 2.0, 3: 2.0}
 
-WEAK_FEATURES_TO_REMOVE = ["nameOrig", "nameDest", "type_DEBIT"]
+WEAK_FEATURES_TO_REMOVE = ["nameOrig", "dest_jump", "type_PAYMENT", "type_CASH_IN", "type_DEBIT", "dest_zero_before", 
+                           "oldbalanceDest", "nameDest", "step", "dest_balance_mismatch", "type_CASH_OUT"]
+
+STRICT_TYPES = ["TRANSFER", "CASH_OUT"]
